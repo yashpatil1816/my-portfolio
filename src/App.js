269ed9c9091d5ca23886e37de87068599fdfe03b
yashpatil1816 <yashpatil2571@gmail.com>
2026-04-18 12:15:17 +0530

    fix build script

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useCallback } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion, AnimatePresence, useScroll } from "framer-motion";
 
 const GlobalStyle = () => (
